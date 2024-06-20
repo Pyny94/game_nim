36650b7e1f3702c9515bfc66ed556c06621d85e5
Vladislav <pyny94@mail.ru>
2024-06-20 21:05:06 +0300

    Add files via upload

--- 04_practice.py
+++ 04_practice.py
@@ -1,26 +1,27 @@
 from nim_engin import get_bunches, put_stones, is_gameover, take_from_bunch
 import os
 
-from termcolor import cprint, colored
+from colorama import Fore,Back,Style
+
 
 
 put_stones()
 user_number = 1
 while True:
-    cprint("Текущая позиция", color='green')
-    cprint(get_bunches(), color='green')
+    print(Fore.YELLOW + 'Текущая позиция' + Style.RESET_ALL)
+    print(Fore.BLUE + str(get_bunches()) + Style.RESET_ALL)
     user_color = 'Blue' if user_number == 1 else 'yellow'
-    cprint("Ход игрока {}".format(user_number), color=user_color)
-    pos = input(colored("Откуда берем?", color=user_color))
-    qua = input(colored('Сколько берем?', color=user_color))
+    print(Fore.GREEN + f'>>> Ход игрока {user_number}' + Style.RESET_ALL)
+    pos = input("Откуда берем?")
+    qua = input('Сколько берем?')
     step_successed =take_from_bunch(position=int(pos), quantity=int(qua))
     if step_successed:
         user_number = 2 if user_number == 1 else 1
     else:
-        cprint('невозможный ход!', color='red')
+        print('невозможный ход!')
     if is_gameover():
         break
 
 
 
-cprint('выйграл игрок номер{}'.format(user_number), color='red')
+print(f'Победил игрок {user_number}!')
